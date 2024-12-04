fed5ad44258fd6304139a41c215e5eb944f73569
kboverhuis <kboverhuis@gmail.com>
2024-12-03 21:47:24 -0600

    Added bootstrap

--- src/App.js
+++ src/App.js
@@ -3,24 +3,26 @@ import './App.css';
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather Project </h1>
-      <footer>
-        This project was created by{" "}
-        <a
-          href="https://www.shecodes.io/graduates/122224-kristi-boverhuis"
-          target="_blank"
-        >
-          {" "}
-          Krist Boverhuis
-        </a>{" "}
-        and is{" "}
-        <a
-          href="https://github.com/kboverhuis/react-weather-app"
-          target="_blank"
-        >
-          open-sourced on GitHub
-        </a>
-      </footer>
+      <div className="container">
+        <h1>Weather Project *</h1>
+        <footer>
+          This project was created by{" "}
+          <a
+            href="https://www.shecodes.io/graduates/122224-kristi-boverhuis"
+            target="_blank"
+          >
+            {" "}
+            Krist Boverhuis
+          </a>{" "}
+          and is{" "}
+          <a
+            href="https://github.com/kboverhuis/react-weather-app"
+            target="_blank"
+          >
+            open-sourced on GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
