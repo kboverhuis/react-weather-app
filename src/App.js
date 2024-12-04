afed35973efc162bc06ebabd2e85f743dc628f28
kboverhuis <kboverhuis@gmail.com>
2024-12-03 21:30:26 -0600

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,7 +1,28 @@
 import './App.css';
 
 export default function App() {
-  return <div className="App">Weather Project</div>;
+  return (
+    <div className="App">
+      <h1>Weather Project </h1>
+      <footer>
+        This project was created by{" "}
+        <a
+          href="https://www.shecodes.io/graduates/122224-kristi-boverhuis"
+          target="_blank"
+        >
+          {" "}
+          Krist Boverhuis
+        </a>{" "}
+        and is{" "}
+        <a
+          href="https://github.com/kboverhuis/react-weather-app"
+          target="_blank"
+        >
+          open-sourced on GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
 
 
