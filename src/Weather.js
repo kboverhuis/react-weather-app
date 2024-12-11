56840fffa5e15510d2a622b907861a4880784c2a
kboverhuis <kboverhuis@gmail.com>
2024-12-10 21:49:46 -0600

    Created component to track temperature unit

--- src/Weather.js
+++ src/Weather.js
@@ -15,7 +15,7 @@ export default function Weather(props) {
     humidity: response.data.temperature.humidity,
     city: response.data.city,
     date: new Date(response.data.time * 1000),
-    icon: `http://shecodes-assets.s3.amazonaws.com/api/weather/icons/${response.data.condition.icon}.png`,
+    icon: response.data.condition.icon,
   });
   }
 
