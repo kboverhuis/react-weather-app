05d7b70d492ed77ee7964e103625c72e0f66406f
kboverhuis <kboverhuis@gmail.com>
2025-01-19 22:39:01 -0600

    Updated Weather.js
    Tried to correct icon not defined error

--- src/Weather.js
+++ src/Weather.js
@@ -23,47 +23,47 @@ export default function Weather(props) {
   }
 
   function search() {
-const apiKey = "8908d7b1834oa44093b7b64af0t393f3";
-let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
-axios.get(apiUrl).then(handleResponse);
+    const apiKey = "8908d7b1834oa44093b7b64af0t393f3";
+    let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}`;
+    axios.get(apiUrl).then(handleResponse);
   }
 
-function handleSubmit(event) {
-  event.preventDefault();
-  search();
-}
+  function handleSubmit(event) {
+    event.preventDefault();
+    search();
+  }
 
-function handleCityChange(event) {
-setCity(event.target.value);
-}
+  function handleCityChange(event) {
+    setCity(event.target.value);
+  }
 
   if (weatherData.ready) {
- return (
-   <div className="Weather">
-     <form onSubmit={handleSubmit}>
-       <div className="row">
-         <div className="col-9">
-           <input
-             type="search"
-             placeholder="Enter a city"
-             className="form-control"
-             autoFocus="on"
-             onChange={handleCityChange}
-           />
-         </div>
-         <div className="col-3">
-           <input type="submit" value="Search" className="btn btn-primary" />
-         </div>
-       </div>
-     </form>
-     <div>
-       <WeatherInfo data={weatherData} />
-       <WeatherForecast coordinates={weatherData.coordinates}/>
-     </div>
-   </div>
- );
+    return (
+      <div className="Weather">
+        <form onSubmit={handleSubmit}>
+          <div className="row">
+            <div className="col-9">
+              <input
+                type="search"
+                placeholder="Enter a city"
+                className="form-control"
+                autoFocus="on"
+                onChange={handleCityChange}
+              />
+            </div>
+            <div className="col-3">
+              <input type="submit" value="Search" className="btn btn-primary" />
+            </div>
+          </div>
+        </form>
+        <div>
+          <WeatherInfo data={weatherData} />
+          <WeatherForecast coordinates={weatherData.coordinates} />
+        </div>
+      </div>
+    );
   } else {
-search();
-return "Loading...";
+    search();
+    return "Loading...";
   }
 }
